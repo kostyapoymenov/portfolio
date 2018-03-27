3bab6ff5bb23a12a6dc4b56e77c85a8c0add048f
kostyapoymenov <kostya6699@mail.ru>
2018-03-27 20:41:49 +0300

    saidbar create #1

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,11 @@
-var autorizationBtn = document.querySelector('.login-btn'),
-    flipperBlock = document.querySelector('.flipper'),
-    onMainBtn = document.querySelector('.main-btn');
+const   autorizationBtn = document.querySelector('.login-btn'),
+        flipperBlock = document.querySelector('.flipper'),
+        onMainBtn = document.querySelector('.main-btn'),
+        blogMobileBtn = document.querySelector('.blog-left__circle'),
+        saidbarBlog = document.querySelector('.blog-left');
 
+
+// Эффект flip
 autorizationBtn.addEventListener('click', function () {
    this.classList.remove('active');
    flipperBlock.classList.add('flip');
@@ -10,4 +14,9 @@ autorizationBtn.addEventListener('click', function () {
 onMainBtn.addEventListener('click', function () {
     flipperBlock.classList.remove('flip');
     autorizationBtn.classList.add('active');
+});
+
+// Сайдбар в "Блог"
+blogMobileBtn.addEventListener('click', function () {
+    this.parentElement().classList.add('active');
 });
